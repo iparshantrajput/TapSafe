9e73cd307ba298ce04b6ada3148a5398fc2f651e
iparshantrajput <parshantrajput97@gmail.com>
2020-12-09 16:20:14 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox1widget/GeneratedIPhone11ProX1Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox2widget/GeneratedIPhone11ProX2Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox3widget/GeneratedIPhone11ProX3Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox4widget/GeneratedIPhone11ProX4Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox5widget/GeneratedIPhone11ProX5Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox6widget/GeneratedIPhone11ProX6Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox7widget/GeneratedIPhone11ProX7Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox8widget/GeneratedIPhone11ProX8Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox9widget/GeneratedIPhone11ProX9Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox10widget/GeneratedIPhone11ProX10Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox11widget/GeneratedIPhone11ProX11Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox12widget/GeneratedIPhone11ProX12Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox13widget/GeneratedIPhone11ProX13Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox14widget/GeneratedIPhone11ProX14Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox15widget/GeneratedIPhone11ProX15Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox16widget/GeneratedIPhone11ProX16Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox17widget/GeneratedIPhone11ProX17Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox18widget/GeneratedIPhone11ProX18Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox25widget/GeneratedIPhone11ProX25Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox26widget/GeneratedIPhone11ProX26Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox27widget/GeneratedIPhone11ProX27Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox20widget/GeneratedIPhone11ProX20Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox36widget/GeneratedIPhone11ProX36Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox37widget/GeneratedIPhone11ProX37Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox38widget/GeneratedIPhone11ProX38Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox39widget/GeneratedIPhone11ProX39Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox21widget/GeneratedIPhone11ProX21Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox22widget/GeneratedIPhone11ProX22Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox23widget/GeneratedIPhone11ProX23Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox24widget/GeneratedIPhone11ProX24Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox35widget/GeneratedIPhone11ProX35Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox30widget/GeneratedIPhone11ProX30Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox29widget/GeneratedIPhone11ProX29Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox31widget/GeneratedIPhone11ProX31Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox32widget/GeneratedIPhone11ProX32Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox33widget/GeneratedIPhone11ProX33Widget.dart';
+import 'package:flutterapp/tapsafeapp/generatediphone11prox34widget/GeneratedIPhone11ProX34Widget.dart';
+
+void main() {
+  runApp(TapSafeApp());
+}
+
+class TapSafeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedIPhone11ProX1Widget',
+      routes: {
+        '/GeneratedIPhone11ProX1Widget': (context) =>
+            GeneratedIPhone11ProX1Widget(),
+        '/GeneratedIPhone11ProX2Widget': (context) =>
+            GeneratedIPhone11ProX2Widget(),
+        '/GeneratedIPhone11ProX3Widget': (context) =>
+            GeneratedIPhone11ProX3Widget(),
+        '/GeneratedIPhone11ProX4Widget': (context) =>
+            GeneratedIPhone11ProX4Widget(),
+        '/GeneratedIPhone11ProX5Widget': (context) =>
+            GeneratedIPhone11ProX5Widget(),
+        '/GeneratedIPhone11ProX6Widget': (context) =>
+            GeneratedIPhone11ProX6Widget(),
+        '/GeneratedIPhone11ProX7Widget': (context) =>
+            GeneratedIPhone11ProX7Widget(),
+        '/GeneratedIPhone11ProX8Widget': (context) =>
+            GeneratedIPhone11ProX8Widget(),
+        '/GeneratedIPhone11ProX9Widget': (context) =>
+            GeneratedIPhone11ProX9Widget(),
+        '/GeneratedIPhone11ProX10Widget': (context) =>
+            GeneratedIPhone11ProX10Widget(),
+        '/GeneratedIPhone11ProX11Widget': (context) =>
+            GeneratedIPhone11ProX11Widget(),
+        '/GeneratedIPhone11ProX12Widget': (context) =>
+            GeneratedIPhone11ProX12Widget(),
+        '/GeneratedIPhone11ProX13Widget': (context) =>
+            GeneratedIPhone11ProX13Widget(),
+        '/GeneratedIPhone11ProX14Widget': (context) =>
+            GeneratedIPhone11ProX14Widget(),
+        '/GeneratedIPhone11ProX15Widget': (context) =>
+            GeneratedIPhone11ProX15Widget(),
+        '/GeneratedIPhone11ProX16Widget': (context) =>
+            GeneratedIPhone11ProX16Widget(),
+        '/GeneratedIPhone11ProX17Widget': (context) =>
+            GeneratedIPhone11ProX17Widget(),
+        '/GeneratedIPhone11ProX18Widget': (context) =>
+            GeneratedIPhone11ProX18Widget(),
+        '/GeneratedIPhone11ProX25Widget': (context) =>
+            GeneratedIPhone11ProX25Widget(),
+        '/GeneratedIPhone11ProX26Widget': (context) =>
+            GeneratedIPhone11ProX26Widget(),
+        '/GeneratedIPhone11ProX27Widget': (context) =>
+            GeneratedIPhone11ProX27Widget(),
+        '/GeneratedIPhone11ProX20Widget': (context) =>
+            GeneratedIPhone11ProX20Widget(),
+        '/GeneratedIPhone11ProX36Widget': (context) =>
+            GeneratedIPhone11ProX36Widget(),
+        '/GeneratedIPhone11ProX37Widget': (context) =>
+            GeneratedIPhone11ProX37Widget(),
+        '/GeneratedIPhone11ProX38Widget': (context) =>
+            GeneratedIPhone11ProX38Widget(),
+        '/GeneratedIPhone11ProX39Widget': (context) =>
+            GeneratedIPhone11ProX39Widget(),
+        '/GeneratedIPhone11ProX21Widget': (context) =>
+            GeneratedIPhone11ProX21Widget(),
+        '/GeneratedIPhone11ProX22Widget': (context) =>
+            GeneratedIPhone11ProX22Widget(),
+        '/GeneratedIPhone11ProX23Widget': (context) =>
+            GeneratedIPhone11ProX23Widget(),
+        '/GeneratedIPhone11ProX24Widget': (context) =>
+            GeneratedIPhone11ProX24Widget(),
+        '/GeneratedIPhone11ProX35Widget': (context) =>
+            GeneratedIPhone11ProX35Widget(),
+        '/GeneratedIPhone11ProX30Widget': (context) =>
+            GeneratedIPhone11ProX30Widget(),
+        '/GeneratedIPhone11ProX29Widget': (context) =>
+            GeneratedIPhone11ProX29Widget(),
+        '/GeneratedIPhone11ProX31Widget': (context) =>
+            GeneratedIPhone11ProX31Widget(),
+        '/GeneratedIPhone11ProX32Widget': (context) =>
+            GeneratedIPhone11ProX32Widget(),
+        '/GeneratedIPhone11ProX33Widget': (context) =>
+            GeneratedIPhone11ProX33Widget(),
+        '/GeneratedIPhone11ProX34Widget': (context) =>
+            GeneratedIPhone11ProX34Widget(),
+      },
+    );
+  }
+}
